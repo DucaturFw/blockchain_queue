d372ad431b9f8199f69970cf75d97c698c2587a3
MaxSvargal <maxsvargal@gmail.com>
2018-07-13 23:51:50 +0300

    Add TSLint, fix issuses

--- neo/main.ts
+++ neo/main.ts
@@ -1,10 +1,11 @@
-import fetch from 'node-fetch'
+import nodeFetch from 'node-fetch'
+import { propOr, zip } from 'ramda'
 import r, { Connection, WriteResult } from 'rethinkdb'
-import { zip, propOr } from 'ramda'
-import { parseExchangeCall, checkTxSuccess } from './vm'
+
+import { checkTxSuccess, parseExchangeCall } from './vm'
 
 type DB = typeof r
-type Fetch = typeof fetch
+type Fetch = typeof nodeFetch
 
 const delay = (time: number) =>
   new Promise(resolve => setTimeout(resolve, time))
@@ -12,7 +13,7 @@ const delay = (time: number) =>
 const prepareFetchJson = (fetch: Fetch, rpcEndpoint: string) => (path: string) =>
   fetch(`${rpcEndpoint}${path}`).then(res => res.json())
 
-const insertIntoDb = (db: DB, conn: Connection) => async (rows: {}[]): Promise<any> => {
+const insertIntoDb = (db: DB, conn: Connection) => async (rows: Array<{}>): Promise<any> => {
   const res = await db.table('transactions').insert(rows).run(conn)
   if (res.errors) throw res.first_error
 }
@@ -20,30 +21,35 @@ const insertIntoDb = (db: DB, conn: Connection) => async (rows: {}[]): Promise<a
 const getPageBasedOnCount = (db: DB, conn: Connection) =>
   db.table('transactions').count().div(15).round().run(conn)
 
-type IteratePage = (address: string, fetchJson: (path: string) => Promise<any>, insertIntoDb: (rows: {}[]) => Promise<WriteResult<any, any>> ) => (page: number) => Promise<number>
-export const iteratePage: IteratePage = (address, fetchJson, insertIntoDb ) => async (page) => {
+type IteratePage = (
+  address: string,
+  fetchJson: (path: string) => Promise<any>,
+  insert: (rows: object[]) => Promise<WriteResult<any, any>> ) =>
+    (page: number) => Promise<number>
+
+export const iteratePage: IteratePage = (address, fetchJson, insert) => async (page) => {
   const txs: ITransaction[] = await fetchJson(`get_last_transactions_by_address/${address}/${page}`)
-  if (!Array.isArray(txs)) throw Error('Fetched tx\'s is not an array')
+  if (!Array.isArray(txs)) throw Error(`Fetched tx\'s is not an array: ${txs}`)
 
   const ids = txs
     .filter(v => v.type === 'InvocationTransaction')
-    .map(v => v.txid)
+    .map((v) => v.txid)
 
-  const transactionsReqs: Promise<ISingleTransaction>[] = ids.map(id => fetchJson(`get_transaction/${id}`))
-  const logsReqs: Promise<IApplogTx>[] = ids.map(id => fetchJson(`tx/${id}`))
+  const transactionsReqs: Array<Promise<ISingleTransaction>> = ids.map((id) => fetchJson(`get_transaction/${id}`))
+  const logsReqs: Array<Promise<IApplogTx>> = ids.map((id) => fetchJson(`tx/${id}`))
 
   const [ transaction, logs ] = await Promise.all([
     Promise.all(transactionsReqs),
-    Promise.all(logsReqs)
+    Promise.all(logsReqs),
   ])
-  
+
   const rows = zip(transaction, logs)
-    .map(v => ({ tx: v[0], log: v[1] }))
+    .map((v) => ({ tx: v[0], log: v[1] }))
     .filter(({ tx }) => propOr(false, 'method', parseExchangeCall(tx.script)) === 'exchange')
     .filter(({ log }) => checkTxSuccess(log))
     .map(({ tx, log }) => ({ id: tx.txid, tx, log }))
 
-  await insertIntoDb(rows)
+  await insert(rows)
 
   // return all transactions without any filters
   return txs.length
